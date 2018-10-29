5b61b9474aa4896114bd1f7fe9ceef989858d80d
verogenoni@hotmail.com <juliandrets@gmail.com>
2018-10-29 17:04:52 -0300

    web

---
+++ index.php
@@ -0,0 +1,187 @@
+<html>
+<head>
+	<title>Outkat</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+	<link rel="stylesheet" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
+	<link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400,400i,700,700i,900" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" /> 
+</head>
+<body>
+
+	<?php include "struct/header.php"; ?>
+
+	<section id="banner">
+		
+	</section>
+
+	<section id="section1">
+		<img src="http://premiumbluethemes.com/Joomla/crypto/images/background/desktop.png" alt="">
+		<article>
+			<h2>Lorem ipsum dolor sit amet.</h2>
+			<div class="bar"></div>
+			<h3>Lorem ipsum dolor sit amet, con amet, consectetur adipisicing elit. ationem adipisci!</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim incidunt, inventore dolor facilis, est impedit autem? Reiciendis nostrum vitae voluptatum? Modi qui necessitatibus facilis aut magni, eligendi dolorem enim quasi.</p>
+			<div class="btn">Join us now</div>
+		</article>
+	</section>
+
+	<section id="servicios">
+		<section>
+			<h2>Why Choose Bitcoin</h2>
+			<h3>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+
+			<ul>
+				<li>	
+					<article>
+						<i class="fa fa-bell-o" aria-hidden="true"></i>
+						<h4>Safe And Secure</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias vero nesciunt neque sit perferendis fugit modi obcaecati nihil delectus!
+						</p>
+					</article>
+				</li>
+				<li>	
+					<article>
+						<i class="fa fa-bell-o" aria-hidden="true"></i>
+						<h4>Safe And Secure</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias vero nesciunt neque sit perferendis fugit modi obcaecati nihil delectus!
+						</p>
+					</article>
+				</li>
+				<li>	
+					<article>
+						<i class="fa fa-bell-o" aria-hidden="true"></i>
+						<h4>Safe And Secure</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias vero nesciunt neque sit perferendis fugit modi obcaecati nihil delectus!
+						</p>
+					</article>
+				</li>
+			</ul>
+			<ul>
+				<li>	
+					<article>
+						<i class="fa fa-bell-o" aria-hidden="true"></i>
+						<h4>Safe And Secure</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias vero nesciunt neque sit perferendis fugit modi obcaecati nihil delectus!
+						</p>
+					</article>
+				</li>
+				<li>	
+					<article>
+						<i class="fa fa-bell-o" aria-hidden="true"></i>
+						<h4>Safe And Secure</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias vero nesciunt neque sit perferendis fugit modi obcaecati nihil delectus!
+						</p>
+					</article>
+				</li>
+				<li>	
+					<article>
+						<i class="fa fa-bell-o" aria-hidden="true"></i>
+						<h4>Safe And Secure</h4>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias vero nesciunt neque sit perferendis fugit modi obcaecati nihil delectus!
+						</p>
+					</article>
+				</li>
+			</ul>
+		</section>
+	</section>
+
+	<section id="about">
+		<article>
+			<h2>About</h2>
+			<div class="bar"></div>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam architecto in expedita quos numquam iusto velit dicta, beatae nisi cumque fugiat, tempora cum cupiditate explicabo ducimus a accusantium blanditiis pariatur.
+			</p>
+
+			<ul>
+				<li><i class="fa fa-check" aria-hidden="true"></i> Donec facilisis velit eu est phasellus consequat quis nostrud</li>
+				<li><i class="fa fa-check" aria-hidden="true"></i> Donec facilisis velit eu est phasellus consequat quis nostrud</li>
+				<li><i class="fa fa-check" aria-hidden="true"></i> Donec facilisis velit eu est phasellus consequat quis nostrud</li>
+				<li><i class="fa fa-check" aria-hidden="true"></i> Donec facilisis velit eu est phasellus consequat quis nostrud</li>
+				<li><i class="fa fa-check" aria-hidden="true"></i> Donec facilisis velit eu est phasellus consequat quis nostrud</li>
+			</ul>
+
+			<div class="btn">Saber más</div>
+			<div class="btn">Contactar</div>
+		</article>
+
+		<img src="http://premiumbluethemes.com/Joomla/crypto/images/about/1.png" alt="">
+	</section>
+
+	<section id="slogan">
+		<h2>Ready To Boost Your Income?</h2>
+
+		<div class="bar"></div>
+
+		<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Perspiciatis unde omnis iste natus error sit.</p>
+
+		<div class="btn">Contactar</div>
+	</section>
+
+	<section id="count">
+		<ul>
+			<li>
+				<figure>
+					<i class="fa fa-check" aria-hidden="true"></i>
+				</figure>
+				<h2>25+</h2>
+				<h3>Support Countries</h3>
+			</li>
+			<li>
+				<figure>
+					<i class="fa fa-check" aria-hidden="true"></i>
+				</figure>
+				<h2>25+</h2>
+				<h3>Support Countries</h3>
+			</li>
+			<li>
+				<figure>
+					<i class="fa fa-check" aria-hidden="true"></i>
+				</figure>
+				<h2>25+</h2>
+				<h3>Support Countries</h3>
+			</li>
+			<li>
+				<figure>
+					<i class="fa fa-check" aria-hidden="true"></i>
+				</figure>
+				<h2>25+</h2>
+				<h3>Support Countries</h3>
+			</li>
+		</ul>
+	</section>
+
+	<section id="contacto">
+		<h2>Contactate con nosotros</h2>
+		<div class="bar"></div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+		<form action="" method="POST">
+			
+			<input type="text" name="" placeholder="Tu nombre *" required>
+			<input type="email" name="" placeholder="Tu email *" required>
+			<input type="phone" name="" placeholder="Tu telefono *" required>
+			<textarea placeholder="Tu mensaje *" required></textarea>
+			
+			<button class="btn">Enviar Consulta</button>
+		</form>
+
+	</section>
+
+	<footer><p>Design by novativex.com</p></footer>
+
+</body>
+</html>
---
+++ struct/header.php
@@ -0,0 +1,19 @@
+<header>
+	<section id="topbar">
+		
+	</section>
+	<nav>
+		<img src="img/logo.png" alt="" class="logo">
+
+		<div class="btn">Get a Quote?</div>
+
+		<ul id="menu-n-responsive">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Pages</a></li>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</nav>
+</header>
